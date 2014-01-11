d002412be72666b2b573e8ed6fde1b8670835b5f
Florida Elago <florida.elago@gmail.com>
2014-01-10 21:25:02 -0600

    add things that I do

---
+++ js/thingsido.js
@@ -0,0 +1,20 @@
+var things_i_do = [
+  "feasting on <a href='http://www.urbanspoon.com/f/332/46139/Winnipeg/Sushi-Restaurants'>sushi</a>",
+  "making lattes",
+  "playing games on <a href='http://steamcommunity.com/profiles/76561198080788913/'>Steam</a>",
+  "reading <a href='http://feedly.com/'>blogs</a>",
+  "reading <a href='https://readmill.com/florida_elago'>books</a>",
+  "chilling in local coffee shops",
+  "learning japanese",
+  "learning more stuff about programming",
+  "discovering new things that interests me",
+  "singing in the shower",
+  "playing guitar",
+  "binge watching on <a href='http://netflix.com'>Netflix</a>"];
+
+function display_things_i_do(){
+  what_i_do = things_i_do[Math.floor(Math.random() * things_i_do.length)];
+  document.getElementById("what_i_do").innerHTML = what_i_do;
+}
+
+display_things_i_do();
