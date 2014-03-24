57bf484394e5b6d5bfdec796a351bd43b85d5110
Florida Elago <florida.elago@gmail.com>
2014-03-23 19:06:04 -0700

    add where I am

---
+++ js/whereami.js
@@ -0,0 +1,22 @@
+function fetchJSONFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === 4) {
+          if (httpRequest.status === 200) {
+              var data = JSON.parse(httpRequest.responseText);
+              if (callback) callback(data);
+          }
+      }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send(); 
+}
+
+fetchJSONFile('https://api.foursquare.com/v2/users/self/checkins?oauth_token=X0OV54PKNCDPWAAGHTIM04HCACPRPWZ0BP3LZKQDUXZMLQ5T&v=20140323', function(data){
+    document.getElementById("whereami").innerHTML = data.response.checkins.items[0].venue.name + 
+    " at " + 
+    data.response.checkins.items[0].venue.location.address.split(" ").splice(1,2).join(" ")
+});
+
+
+
