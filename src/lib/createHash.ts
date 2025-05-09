131865f387e8756afb3b4ceffd54679742a68e30
florida <florida@default.com>
2025-05-09 09:03:11 -0400

    hash update

--- src/lib/createHash.ts
+++ src/lib/createHash.ts
@@ -1,7 +1,9 @@
-export async function createHash(message: string) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(message);
-    const hash = await crypto.subtle.digest("SHA-256", data);
-    return hash;
-  }
-  
+async function createHash(message: string) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  return hashHex;
+}
