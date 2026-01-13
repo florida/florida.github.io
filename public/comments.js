8ac766c76faba14a2dce28e3533f8715a09a8225
florida <florida@default.com>
2026-01-12 19:24:47 -0500

    chore: update

--- public/comments.js
+++ public/comments.js
@@ -9,7 +9,10 @@ const makeIframeContent = (target) => {
   <head>
     <link rel="stylesheet" href="${cssPath}">
     <link rel="stylesheet" href="${customCSSPath}">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
     <base target="_parent" />
     <link>
     <script>
