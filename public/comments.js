fe7b91a8b3e808406883913573290cb12255b68b
florida <florida@default.com>
2026-01-12 20:07:43 -0500

    update css path

--- public/comments.js
+++ public/comments.js
@@ -2,7 +2,7 @@ window.CUSDIS = {};
 const makeIframeContent = (target) => {
   const host = target.dataset.host || "https://cusdis.com";
   const iframeJsPath = target.dataset.iframe || `${host}/js/iframe.umd.js`;
-  const cssPath = `comments.css`;
+  const cssPath = `/comments.css`;
   return `<!DOCTYPE html>
 <html>
   <head>
